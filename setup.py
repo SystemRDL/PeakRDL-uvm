5a15ecdf8cc04544f417ca626d1cb0e006ca60d2
Alex Mykyta <amykyta3@users.noreply.github.com>
2020-12-09 22:16:44 -0800

    Update classifier

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3 :: Only",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
